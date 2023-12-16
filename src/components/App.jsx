6dfa172b8283743f49200367f3accc383b51429a
Artem <krylov.artem.1995@gmail.com>
2023-12-16 16:46:26 +0100

    Update hooks in project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,59 @@
+import React, { useState } from 'react';
+import css from './App.module.css';
+
+import { Feedback } from './Feetback/feetback';
+import { Notification } from './Notification/Notification';
+import { Section } from './Section/Section';
+import { Statistic } from './Statistic/Statistic';
+
 export const App = () => {
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const countTotalFeedback = () => {
+    return feedback.good + feedback.neutral + feedback.bad;
+  };
+
+  const countPositiveFeedbackPercentage = () => {
+    return Math.round((feedback.good / countTotalFeedback()) * 100);
+  };
+
+  const handleIncrement = event => {
+    event.preventDefault();
+    const name = event.target.name;
+    setFeedback(prevFeedback => ({
+      ...prevFeedback,
+      [name]: prevFeedback[name] + 1,
+    }));
+  };
+
+  const totalFeedback = countTotalFeedback();
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <section className={css.wrapper}>
+      <Section title="Please save feedback">
+        <Feedback
+          options={Object.keys(feedback)}
+          onLeaveFeedback={handleIncrement}
+        />
+      </Section>
+
+      {totalFeedback ? (
+        <Section title="Statistic">
+          <Statistic
+            good={feedback.good}
+            neutral={feedback.neutral}
+            bad={feedback.bad}
+            total={totalFeedback}
+            positivePercentage={countPositiveFeedbackPercentage()}
+          />
+        </Section>
+      ) : (
+        <Notification message="There is no feedback" />
+      )}
+    </section>
   );
 };
